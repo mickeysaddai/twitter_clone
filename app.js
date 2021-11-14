6b29fe29f900f10e97bb01e54aa3ff453a6a7741
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-11-13 23:19:26 -0500

    routes

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 const express = require("express");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 const app = express();
 
 const db = require('./config/keys').mongoURI;
 
 app.get("/", (req, res) => res.send("Hello"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 const port = process.env.PORT || 5000;
-
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
