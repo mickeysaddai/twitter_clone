d40c965a0ac80a3dbd38cd8ebace35fc2a2aa116
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-11-14 11:55:17 -0500

    validations in progress

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
-const mongoose = require('mongoose');
 const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const User = require("./models/User")
 const bodyParser = require('body-parser');
-const app = express();
-const db = require('./config/keys').mongoURI;
+const passport = require('passport');
 
+// const { MongoClient } = require('mongodb');
+// const uri = 
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect().then((response) => {
+//     console.log("CONNECTED!!!!")
+//     console.log(response)
+// }).catch((err) => {
+//     console.log(err)
+// })
+// client.connect(err => {
+//   const collection = client.db("twitter-clone").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 mongoose
-.connect(db, { useNewUrlParser: true})
-.then(() =>console.log("Connected to mongoDB"))
+.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Connected to mongoDB"))
 .catch(err => console.log(err))
 
+app.use(bodyParser.urlencoded({ extended: false }));  //respond to req from other software like postman
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello Mickey"));
+app.get("/", (req, res) => {
+    const user = new User({
+        handle: "mic",
+        email: "mickey@gmail.com", 
+        password: "mickey123"
+    })
+    user.save()
+    res.send("Hi Everyone")
+})
+// app.get("/", (req, res) => res.send("Hello Mickey"));
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+
 
 
 const port = process.env.PORT || 5000;
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,11 +2,30 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const keys = require('../../config/keys');
+const jwt = require('jsonwebtoken')
+const passport = require('passport');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
+
+// You may want to start commenting in information about your routes so that you can find the appropriate ones quickly.
+router.get(
+  '/current',
+   passport.authenticate('jwt', {session: false}), 
+   (req, res) => {
+  res.send(req.user);
+})
+
+
+
+
 router.post('/register', (req, res) => {
+
+  console.log(req.body)
   // Check to make sure nobody has already registered with a duplicate email
   User.findOne({ email: req.body.email })
     .then(user => {
@@ -21,17 +40,53 @@ router.post('/register', (req, res) => {
           password: req.body.password
         })
 
+        // newUser.save().then(user => res.send(user)).catch(err => res.send(err));
         bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(newUser.password, salt, (err, hash) => {
             if (err) throw err;
             newUser.password = hash;
             newUser.save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
           })
         })
       }
     })
+    
+ })
+ 
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({email})
+   .then(user => {
+     if (!user) {
+       return res.status(404).json({email: 'This user does not exist'});
+     }
+     bcrypt.compare(password, user.password)
+       .then(isMatch => {
+         if (isMatch) {
+           const payload = {
+             id: user.id,
+             handle: user.handle,
+             email: user.email
+           }
+           jwt.sign(
+             payload,
+             keys.secretOrKey,
+             {expiresIn: 3600},
+             (err, token) => {
+               res.json({
+                 success: true,
+                 token: "Bearer" + token
+               });
+             }
+           )
+         } else {
+           return res.status(400).json({password: 'Incorrect password'});
+         }
+       })
+   })
 })
 
 
