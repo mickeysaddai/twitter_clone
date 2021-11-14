4ac54b1f3d64210b174693f0dccab64f01413b49
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-11-13 23:39:57 -0500

    monogoUri updated

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 const app = express();
-
 const db = require('./config/keys').mongoURI;
 
-app.get("/", (req, res) => res.send("Hello"));
+mongoose
+.connect(db, { useNewUrlParser: true})
+.then(() =>console.log("Connected to mongoDB"))
+.catch(err => console.log(err))
+
+
+app.get("/", (req, res) => res.send("Hello Mickey"));
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 app.use(bodyParser.urlencoded({ extended: false }));
